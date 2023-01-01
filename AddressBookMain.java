ff220eea27da3d7b683b7fa783ae0350a3c99943
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-01 18:23:26 +0530

    Added : This is Address Book Main class where Address book class calls.

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,7 @@ package com.blz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
+        AddressBook addressBook = new AddressBook();
+        addressBook.createContact();
     }
 }
