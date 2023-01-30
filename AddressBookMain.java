05981a60af65c8e5d4c4184a5a276702a47711fe
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-30 14:38:30 +0530

    Added : We ask user to add new contact, if not then quit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,39 @@
 package com.blz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
-        AddressBook addressBook = new AddressBook();
-        addressBook.createContact();
+
+        Character ch = 'a';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("***** Welcome to Address Book program *****");
+
+        while(!ch.equals('q')) {
+
+            System.out.print("\na) Add \nq) Quit \nEnter a character: ");
+            ch = scanner.next().charAt(0);
+
+            switch (ch) {
+                case 'a':
+                    AddressBook addressBook = new AddressBook();
+                    addressBook.createContact();
+                    System.out.println("Added person's details : ");
+                    addressBook.displayDetails();
+                    break;
+
+                case 'q':
+                    break;
+
+                default:
+                    System.out.println("Please enter a valid character");
+                    break;
+
+            }
+        }
+
+        scanner.close();
     }
 }
