9519bff0120ff7c6eddbdaf5115b450a5ed90065
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-30 14:58:12 +0530

    Added : We edit the user details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,35 +5,7 @@ import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
-
-        Character ch = 'a';
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("***** Welcome to Address Book program *****");
-
-        while(!ch.equals('q')) {
-
-            System.out.print("\na) Add \nq) Quit \nEnter a character: ");
-            ch = scanner.next().charAt(0);
-
-            switch (ch) {
-                case 'a':
-                    AddressBook addressBook = new AddressBook();
-                    addressBook.createContact();
-                    System.out.println("Added person's details : ");
-                    addressBook.displayDetails();
-                    break;
-
-                case 'q':
-                    break;
-
-                default:
-                    System.out.println("Please enter a valid character");
-                    break;
-
-            }
-        }
-
-        scanner.close();
+        AddressBook addressBook = new AddressBook();
+        addressBook.createContact();
     }
 }
