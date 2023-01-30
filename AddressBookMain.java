4b714fc7ed2bccf536e08ebe1dcd700d8ba20a1c
vishalchandra736 <vishalchandra736@gmail.com>
2023-01-30 15:23:08 +0530

    Added : We delete the user details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,4 @@
 package com.blz.addressbook;
-
-import java.util.Scanner;
-
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book");
